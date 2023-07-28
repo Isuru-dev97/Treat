a47a51520f017df1d66b633f1532598a72a08b6e
Isuru-dev97 <116330611+Isuru-dev97@users.noreply.github.com>
2023-07-28 17:00:33 +0530

    Adding_Packages

--- traetnativeapplication/ios/TraetNativeApplication/AppDelegate.mm
+++ traetnativeapplication/ios/TraetNativeApplication/AppDelegate.mm
@@ -1,11 +1,19 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>
 #import <React/RCTBundleURLProvider.h>
+@import FirebaseCore;
+@import UIKit;
 
 @implementation AppDelegate
 
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    // Add me --- \/
+  [FIRApp configure];
+  return YES;
+  // Add me --- /\
+  // ...
   self.moduleName = @"TraetNativeApplication";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
